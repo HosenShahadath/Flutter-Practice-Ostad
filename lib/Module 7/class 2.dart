8d333e52dd994f150c4460fe532e00138c5cd438
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-01-15 22:16:16 +0600

    class 2

---
+++ lib/Module 7/class 2.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp2());
+}
+
+class MyApp2 extends StatelessWidget{
+  const MyApp2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Demo App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Demo App"),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Container(
+          child: Center(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Text("Text-1"),
+                  Text("Text-2"),
+                  Text("Text-3"),
+                  Text("Text-4"),
+                  Text("Text-5"),
+                  Text("Text-1"),
+                  Text("Text-2"),
+                  Text("Text-3"),
+                  Text("Text-4"),
+                  Text("Text-5"),
+                  Text("Text-1"),
+                  Text("Text-2"),
+                  Text("Text-3"),
+                  Text("Text-4"),
+                  Text("Text-5"),
+                  Text("Text-1"),
+                  Text("Text-2"),
+                  Text("Text-3"),
+                  Text("Text-4"),
+                  Text("Text-5"),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
